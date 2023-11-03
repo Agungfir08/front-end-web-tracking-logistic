9843c141bafd09bcfbc80e13a66e3e35e5a2ab54
arielcatur <arielcatur1305@gmail.com>
2023-11-03 09:43:22 +0800

    merge main

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
