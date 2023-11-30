83366a5c40d2ba929e50acc47afc0ac06ac08183
arielcatur <arielcatur1305@gmail.com>
2023-11-30 14:08:10 +0800

    update order detail

--- src/components/order/OrderInfo.jsx
+++ src/components/order/OrderInfo.jsx
@@ -9,30 +9,39 @@ function OrderInfo() {
   const data = queryClient.getQueryData(["OrderInfo", order.orderNo]);
 
   const {
-    customerId,
-    customerName,
-    customerAddress,
+    refNumber,
+    OrderNo,
+    orderDate,
+    commodity,
+    remarks,
     receiverName,
     receiverAddress,
   } = data?.data || {};
 
   const infoItems = [
-    { name: "Customer Id", value: customerId },
-    { name: "Customer Name", value: customerName },
-    { name: "Customer Address", value: customerAddress },
+    { name: "Ref Number", value: refNumber },
+    { name: "order number", value: OrderNo },
+    { name: "order date", value: orderDate },
+    { name: "commodity", value: commodity },
+    { name: "remarks", value: remarks },
     { name: "Receiver Name", value: receiverName },
     { name: "Receiver Address", value: receiverAddress },
   ];
 
   return (
     <div className="bg-white rounded-lg p-6 flex flex-col justify-between sm:w-[310px] sm:h-[400px] w-[375px] h-[440px]">
-      <h1 className="  sm:text-base text-lg text-[--maincolor] font-semibold">
+      {/* <h1 className="  sm:text-base text-lg text-[--maincolor] font-semibold">
         {order.orderNo}
-      </h1>
+      </h1> */}
       <div className=" flex flex-col gap-4">
-        {infoItems.map((item, index) => (
+        {infoItems.slice(0, -2).map((item, index) => (
           <InfoItem key={index} value={item.value} name={item.name} />
         ))}
+        <div className="flex flex-rows gap-6">
+        {infoItems.slice(-2).map((item, index) => (
+            <InfoItem key={index} name={item.name} value={item.value} />
+          ))}
+        </div>
       </div>
     </div>
   );
